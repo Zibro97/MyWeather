b8eea8fbb4d1ff6052072e3fc90e0f1abe563604
zibro <jgh6272@naver.com>
2022-03-15 21:12:01 +0900

    22/03/15 google Map Api 사용해 구글 지도 띄우기 완료.

--- app/src/main/java/com/example/myweather/view/fragment/MapFragment.kt
+++ app/src/main/java/com/example/myweather/view/fragment/MapFragment.kt
@@ -5,12 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.myweather.BuildConfig
 import com.example.myweather.R
 import com.example.myweather.databinding.FragmentMapBinding
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapView
 import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.*
+import java.lang.AssertionError
+import java.net.MalformedURLException
+import java.net.URL
+import java.util.*
 
+//날씨를 지도로 표현해 보여주는 Fragment
 class MapFragment : Fragment(),OnMapReadyCallback {
 
     private var _binding : FragmentMapBinding? = null
@@ -22,19 +30,11 @@ class MapFragment : Fragment(),OnMapReadyCallback {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val layout = inflater.inflate(R.layout.fragment_map,container,false)
-        mapView = layout.findViewById(R.id.mapFragment) as MapView
+        val rootView = inflater.inflate(R.layout.fragment_map,container,false)
+        mapView = rootView.findViewById(R.id.mapFragment) as MapView
+        mapView.onCreate(savedInstanceState)
         mapView.getMapAsync(this)
-        return layout
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-    }
-    private fun setUpGoogleMap(){
-        mapView = childFragmentManager.findFragmentById(binding.mapFragment.id) as MapView
-        mapView.getMapAsync(this)
+        return rootView
     }
 
     override fun onDestroyView() {
@@ -42,7 +42,59 @@ class MapFragment : Fragment(),OnMapReadyCallback {
         _binding = null
     }
 
-    override fun onMapReady(p0: GoogleMap) {
+    override fun onMapReady(map: GoogleMap) {
+        val point = LatLng( 37.414655, 126.879974)
+        map.addMarker(MarkerOptions().position(point).title("여기"))
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(point,11f))
 
+        val tileProvider = object : UrlTileProvider(256,256){
+            override fun getTileUrl(x: Int, y: Int, zoom: Int): URL? {
+                val s = String.format(Locale.KOREA,"http://tile.openweathermap.org/map/precipitation/%d/%d/%d.png?appid={${BuildConfig.OPEN_WEATHER_API_KEY}}",zoom,x,y)
+
+                if(!checkTileExists(x,y,zoom)) return null
+                try {
+                    return URL(s)
+                }catch (e:MalformedURLException){
+                    throw AssertionError(e)
+                }
+            }
+            private fun checkTileExists(x:Int,y:Int,zoom:Int):Boolean{
+                val minZoom = 12
+                val maxZoom = 16
+
+                return !(zoom<minZoom || zoom>maxZoom)
+            }
+        }
+        map.addTileOverlay(TileOverlayOptions().tileProvider(tileProvider))
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
     }
 }
